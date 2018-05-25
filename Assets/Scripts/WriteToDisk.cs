aca08b3d2ca98afe970b61b11de2deac5c15ac94
Kevin van der Bij <kevinvdbij@gmail.com>
2018-05-25 14:54:41 +0200

    Changed WriteToDisk script

--- Assets/Scripts/WriteToDisk.cs
+++ Assets/Scripts/WriteToDisk.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum WinState
+{
+    PLAYER1,
+    PLAYER2,
+    TIE
+}
+
 public class WriteToDisk : MonoBehaviour
 {
     public static string[] output = new string[1000];
@@ -9,5 +16,10 @@ public class WriteToDisk : MonoBehaviour
     public static void WriteRoundToDisk()
     {
         System.IO.File.WriteAllLines(@"C:\Users\Game Developer\AppData\Local\PokerOpdracht\output.txt", output);
+    }
+    
+    public static void AddOutput(int round, WinState state)
+    {
+        output[round] = "Round" + round + " " + state;
     }
 }

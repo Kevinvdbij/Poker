c660bdec2e64651720e7ff7e994bea07e358c1eb
Kevinvdbij <kevinvdbij@gmail.com>
2018-05-25 12:42:36 +0100

    Initial Commit

---
+++ Assets/Scripts/WriteToDisk.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WriteToDisk : MonoBehaviour
+{
+    public static string[] output = new string[1000];
+
+    public static void WriteRoundToDisk()
+    {
+        System.IO.File.WriteAllLines(@"C:\Users\Game Developer\AppData\Local\PokerOpdracht\output.txt", output);
+    }
+}
